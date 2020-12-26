51886179be4d084dba67b54092f4e3bfcc9db4ea
Shahin Mammadov <s.mammadov@jacobs-university.de>
2020-12-26 23:06:03 +0100

    added static files

--- carselling/settings.py
+++ carselling/settings.py
@@ -119,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIRm 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'carselling/static'),
+]
